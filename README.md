677f2913947b8d7721c9128018d189f1f958122d
Rafael <rafael.merlomendes@gmail.com>
2020-08-06 18:14:52 -0300

    Parte final do 4º dia da NLW.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # NLW-2
 Segundo evento da Rocketseat a Next Level Week
+
+
+## Instalação:
+
+``
+Node.js
+``
+
+```sh
+	npm install express
+	npm install nodemon -D
+
+	npm run dev
+	 ou
+	node src/server.js
+
+	No navegador abra:
+	localhost:5500
+	 ou
+	127.0.0.1:5500
+
+	CTRL+c (fecha o servidor)
+```
--- src/server.js
+++ src/server.js
@@ -1,5 +1,98 @@
-require('express')()
-.get("/", (req, res) =>{
-    return res.send("Hi Lorena!")
+const proffys = [
+    { 
+        name: "Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "40029822", 
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: "20", 
+        weekday: [0],
+        time_from: [720], 
+        time_to: [1920]
+    },
+
+    { 
+        name: "Eu mesmo",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "40029822", 
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: "20", 
+        weekday: [1],
+        time_from: [720], 
+        time_to: [1920]
+    }
+]
+
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
+function getSubject(subjectNumber){
+    const position = +subjectNumber - 1
+    return subjects[position]
+}
+
+function pageLandig(req, res){
+    return res.render("index.html")
+}
+
+function pageStudy(req, res){
+    const filters = req.query
+    return res.render("study.html", {proffys, filters, subjects, weekdays})
+}
+
+function pageGiveClasses(req, res){
+    const data = req.query
+
+    const isNotEmpety = Object.keys(data).length > 0
+    if (isNotEmpety) {
+
+        data.subject = getSubject(data.subject)
+
+        proffys.push(data)
+
+        return res.redirect("/study")
+    }
+
+    return res.render("give-classes.html", {subjects, weekdays})
+}
+
+const express = require('express')
+const server = express()
+
+//Configurar Nunjucks
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
 })
+
+server
+//Configurar arquivos estáticos
+.use(express.static("public"))
+//Rotas da aplicação
+.get("/", pageLandig)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+
 .listen(5500)

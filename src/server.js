8f55f34ff38df33aeab6acf6372e505a8d1971a5
Rafael <rafael.merlomendes@gmail.com>
2020-08-09 18:52:21 -0300

    Parte final da NLW#2.

--- src/server.js
+++ src/server.js
@@ -1,36 +1,12 @@
-const {
-    subjects,
-    weekdays,
-    getSubject
-} = require('./utils/format')
-
-function pageLandig(req, res){
-    return res.render("index.html")
-}
-
-function pageStudy(req, res){
-    const filters = req.query
-    return res.render("study.html", {proffys, filters, subjects, weekdays})
-}
-
-function pageGiveClasses(req, res){
-    const data = req.query
-
-    const isNotEmpety = Object.keys(data).length > 0
-    if (isNotEmpety) {
-
-        data.subject = getSubject(data.subject)
-
-        proffys.push(data)
-
-        return res.redirect("/study")
-    }
-
-    return res.render("give-classes.html", {subjects, weekdays})
-}
-
 const express = require('express')
 const server = express()
+
+const {
+    pageLandig,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+} = require('./pages')
 
 //Configurar Nunjucks
 const nunjucks = require('nunjucks')
@@ -40,11 +16,13 @@ nunjucks.configure('src/views', {
 })
 
 server
+.use(express.urlencoded({extended: true}))
 //Configurar arquivos estáticos
 .use(express.static("public"))
 //Rotas da aplicação
 .get("/", pageLandig)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 
 .listen(5500)
